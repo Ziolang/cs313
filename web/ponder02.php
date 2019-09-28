d1278757aab1ecd9b4e8417953f8dbcdec4ae28e
ziolang <stumpyfletcher@gmail.com>
2019-09-27 23:31:00 -0600

    testing add

--- web/ponder02.php
+++ web/ponder02.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="ponder02.css">
 	</head>
 	<body>
+		<div class="header"></div>
+		<div class="row"></div>
 		<?php
-			echo date("l jS \of F Y h:i:s A");
+			echo "Server time is: " . date("l jS \of F Y h:i:s A");
 		?>
 		<div></div>
 	</body>
