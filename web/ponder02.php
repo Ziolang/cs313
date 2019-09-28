0e62ba8913ee7000830b6cf6a9ff121aaa72d778
ziolang <stumpyfletcher@gmail.com>
2019-09-27 23:54:09 -0600

    testing add

--- web/ponder02.php
+++ web/ponder02.php
@@ -4,8 +4,12 @@
 		<link rel="stylesheet" type="text/css" href="ponder02.css">
 	</head>
 	<body>
-		<div class="header"></div>
-		<div class="row"></div>
+		<div class="row">
+			<img source="beach.jpg"/>
+			<h1>Adam Fletcher></h1><br/>
+			<p>A young father</p>
+			<img class="right" source="beach2.jpg"/>
+		</div>
 		<?php
 			echo "Server time is: " . date("l jS \of F Y h:i:s A");
 		?>
