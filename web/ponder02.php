311b17b8b96273726a7cce6781eb49966f73d89b
ziolang <stumpyfletcher@gmail.com>
2019-09-28 23:11:21 -0600

    added ponder 2

--- web/ponder02.php
+++ web/ponder02.php
@@ -5,13 +5,37 @@
 	</head>
 	<body>
 		<div class="row">
-			<img source="https://www.dropbox.com/s/bxmclkeazzcwrvh/beach.jpg?dl=0" alt="beach"/>
-			<h1>Adam Fletcher</h1> <img class="right" source="https://www.dropbox.com/s/j5mkoij3m0nwc3f/beach2.jpg?dl=0" alt="beach2"/><br/>
-			<p>A young father</p>
+			<h1>Adam Fletcher</h1>
+			<p>Some of my favorite things.</p>
+		</div>
+		<div class="pcontainer">
+			<div class="popup">
+				<img class="fit" src="zach.png" alt="lonso">
+				<span class="centered">Zachary, the First One</span>
+			</div>
+			<div class="popup">
+				<img class="fit" src="beach.png" alt="zach">
+				<span class="centered">The best wife</span>
+			</div>
+			<div class="popup">
+				<img class="fit" src="zoo.png" alt="zoo">
+				<span class="centered">Always love Zoos</span>
+			</div>
+			<div class="popup">
+				<img class="fit" src="dino.png" alt="dino">
+				<span class="centered">DINOSAURS</span>
+			</div>
+			<div class="popup">
+				<img class="fit" src="cupcake.png" alt="cupcake">
+				<span class="centered">Family Crafts and Halloween</span>
+			</div>
+			<div class="popup">
+				<img class="fit" src="benji.png" alt="benji">
+				<span class="centered">Benji, the Second One</span>
+			</div>
 		</div>
 		<?php
 			echo "Server time is: " . date("l jS \of F Y h:i:s A");
 		?>
-		<div></div>
 	</body>
 </html>
