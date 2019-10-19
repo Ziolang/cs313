3c9cf906d1f259e0226f6799309338afb29b8353
ziolang <stumpyfletcher@gmail.com>
2019-10-19 12:10:23 -0600

    in progress

--- web/week05/unit.php
+++ web/week05/unit.php
@@ -29,40 +29,7 @@
 				
 				foreach ($db->query("SELECT * FROM Units WHERE Unit.id =" . $_GET['id']) as $unit) {
 					echo "<h1>" . $unit['name'] . "</h1><br/>Level: " . $unit['lvl'] . "<br/>Experience: " . $unit['exp'] . "</b>Class: " . $unit['class'] . "<br/>Weapon: ";
-					foreach ($db->query("SELECT * FROM Equips WHERE Equips.id =" . $unit['weapon1']) as $w1) {
-						echo $w1['name'] . " (" . $w1['effect'] . ")";
-					}
-					if ($unit['weapon2'] != NULL)
-						foreach ($db->query("SELECT * FROM Equips WHERE Equips.id =" . $unit['weapon2']) as $w2) {
-							echo "<br/>Weapon2: " . $w2['name'] . " (" . $w2['effect'] . ")";
-						}
-					foreach ($db->query("SELECT * FROM Equips WHERE Equips.id =" . $unit['armor']) as $ar) {
-						echo "<br/>Armor: " . $ar['name'] . " (" . $ar['effect'] . ")";
-					}
-					foreach ($db->query("SELECT * FROM Equips WHERE Equips.id =" . $unit['armor']) as $ac) {
-						echo "<br/>Accessory: " . $ac['name'] . " (" . $ac['effect'] . ")";
-					}
-					foreach ($db->query("SELECT * FROM SkillSet WHERE SkillSet.id =" . $unit['A_Ability']) as $aa) {
-						echo "<br/>A-Ability: " . $aa['name'];
-
-						foreach ($db->query("SELECT * FROM Skills WHERE Skills.id =" . $aa['skill1']) as $sk1) {
-							echo "<br/>+ " . $sk1['name'] . ": ";
-							if ($sk1['stat'] != NULL)
-								echo $sk1['dmg'] . " " . $sk1['stat'] . " Damage. ";
-							if ($sk1['effect'] != NULL)
-								echo $sk1['effect'] . " ";
-							echo "Range: " . $sk1['range'] . ". " . $sk1['mp'] . " MP.";
-						}
-
-						foreach ($db->query("SELECT * FROM Skills WHERE Skills.id =" . $aa['skill2']) as $sk2) {
-							echo "<br/>+ " . $sk2['name'] . ": ";
-							if ($sk2['stat'] != NULL)
-								echo $sk2['dmg'] . " " . $sk2['stat'] . " Damage. ";
-							if ($sk2['effect'] != NULL)
-								echo $sk2['effect'] . " ";
-							echo "Range: " . $sk2['range'] . ". " . $sk2['mp'] . " MP.";
-						}
-					}
+					
 					
 				}
 				?>
