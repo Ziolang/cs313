2c60945970db4b66ce696bbdc02d122ac656fe46
ziolang <stumpyfletcher@gmail.com>
2019-10-19 12:22:00 -0600

    in progress

--- web/week05/unit.php
+++ web/week05/unit.php
@@ -42,10 +42,26 @@
 					foreach ($db->query("SELECT * FROM Equips WHERE Equips.id =" . $unit['armor']) as $ac) {
 						echo "<br/>Accessory: " . $ac['name'] . " (" . $ac['effect'] . ")";
 					}
-					foreach ($db->query("SELECT * FROM SkillSet WHERE SkillSet.id =" . $unit['A_Ability']) as $aa) {
+					foreach ($db->query("SELECT * FROM SkillSet WHERE SkillSet.id =" . $unit['A_Abilitiy']) as $aa) {
 						echo "<br/>A-Ability: " . $aa['name'];
 
-						
+						foreach ($db->query("SELECT * FROM Skills WHERE Skills.id =" . $aa['skill1']) as $sk1) {
+							echo "<br/>+ " . $sk1['name'] . ": ";
+							if ($sk1['stat'] != NULL)
+								echo $sk1['dmg'] . " " . $sk1['stat'] . " Damage. ";
+							if ($sk1['effect'] != NULL)
+								echo $sk1['effect'] . " ";
+							echo "Range: " . $sk1['range'] . ". " . $sk1['mp'] . " MP.";
+						}
+
+						foreach ($db->query("SELECT * FROM Skills WHERE Skills.id =" . $aa['skill2']) as $sk2) {
+							echo "<br/>+ " . $sk2['name'] . ": ";
+							if ($sk2['stat'] != NULL)
+								echo $sk2['dmg'] . " " . $sk2['stat'] . " Damage. ";
+							if ($sk2['effect'] != NULL)
+								echo $sk2['effect'] . " ";
+							echo "Range: " . $sk2['range'] . ". " . $sk2['mp'] . " MP.";
+						}
 					}
 					
 				}
