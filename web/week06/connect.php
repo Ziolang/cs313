8537fec8ca2ab10687e28cb3e01a4e673561eb8c
ziolang <stumpyfletcher@gmail.com>
2019-10-22 08:33:19 -0600

    stuff and things

---
+++ web/week06/connect.php
@@ -0,0 +1,21 @@
+<?php
+function get_db() {
+	$db = NULL;
+	try {
+		
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+	return $db;
+}
+?>
---
+++ web/week06/viewScripture.php
@@ -0,0 +1,45 @@
+<?php
+/**********************************************************
+* File: viewScriptures.php
+* Author: Br. Burton
+* 
+* Description: This file shows an example of how to query a
+*   PostgreSQL database from PHP.
+***********************************************************/
+require "connect.php";
+$db = get_db();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Scripture List</title>
+</head>
+
+<body>
+<div>
+
+<h1>Scripture Resources</h1>
+
+<?php
+
+$statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
+$statement->execute();
+// Go through each result
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	// The variable "row" now holds the complete record for that
+	// row, and we can access the different values based on their
+	// name
+	$book = $row['book'];
+	$chapter = $row['chapter'];
+	$verse = $row['verse'];
+	$content = $row['content'];
+	echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+}
+?>
+
+
+</div>
+
+</body>
+</html>
