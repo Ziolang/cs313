0e8f0dd6128b2d1ead14f7f643c0364a007f6e83
ziolang <stumpyfletcher@gmail.com>
2019-10-22 09:44:32 -0600

    stuff and things

--- web/week06/result.php
+++ web/week06/result.php
@@ -40,5 +40,19 @@
         echo '"' . $row['content'] . '"<br><br>';
 	}
 
+foreach ($db->query("SELECT scripture.*, topic.*, ScriptureToTopics.*
+				FROM scripture 
+				JOIN ScriptureToTopics 
+				ON scripture.id = ScriptureToTopics.scripture_id 
+				JOIN topic
+				ON ScriptureToTopics.topic_id = topic.id") as $row) {
+	echo "<b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</b> <br/>Topics: ";
+	foreach ($db->query("SELECT *
+				FROM topic WHERE topic.id = " . $row['topic_id']) as $topic) {
+			echo $topic['name'] . " ";
+		}
+        echo '"' . $row['content'] . '"<br><br>';
+}
+
 ?>
 </body>
