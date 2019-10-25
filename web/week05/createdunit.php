828c9c37dfdd06d6c79404fda4ae1777a3bbd84f
ziolang <stumpyfletcher@gmail.com>
2019-10-25 15:45:24 -0600

    more work

--- web/week05/createdunit.php
+++ web/week05/createdunit.php
@@ -1,96 +1,95 @@
 <?php include "connect.php";
 
 
-				$db = connect();
+	$db = connect();
 
-				$name = $_POST['name'];
-				$class = $_POST['class'];
-				$aa = $_POST['aa'];
-				$sa = "PENDING";
-				$lvl = 1;
-				$exp = 0;
-				$hp = 25; $mp = 15; $atk = 4; $def = 3; $int = 2; 
-				$spr = 2; $move = 3; $crit = 2; $eva = 2;
+	$name = $_POST['name'];
+	$class = $_POST['class'];
+	$aa = $_POST['aa'];
+	$lvl = 1;
+	$exp = 0;
+	$hp = 25; $mp = 15; $atk = 4; $def = 3; $int = 2; 
+	$spr = 2; $move = 3; $crit = 2; $eva = 2;
 
-				if ($_POST['name'] == "ra"){
-					$hp = 20; $mp = 20; $atk = 4; $def = 2; $int = 2; 
-					$spr = 3; $move = 2; $crit = 2; $eva = 3;
-				}
-				if ($_POST['name'] == "sm"){
-					$hp = 20; $mp = 20; $atk = 2; $def = 3; $int = 4; 
-					$spr = 2; $move = 2; $crit = 1; $eva = 4;
-				}
-				if ($_POST['name'] == "om"){
-					$hp = 15; $mp = 25; $atk = 2; $def = 2; $int = 4; 
-					$spr = 3; $move = 2; $crit = 4; $eva = 1;
-				}
+	if ($_POST['name'] == "ra"){
+		$hp = 20; $mp = 20; $atk = 4; $def = 2; $int = 2; 
+		$spr = 3; $move = 2; $crit = 2; $eva = 3;
+	}
+	if ($_POST['name'] == "sm"){
+		$hp = 20; $mp = 20; $atk = 2; $def = 3; $int = 4; 
+		$spr = 2; $move = 2; $crit = 1; $eva = 4;
+	}
+	if ($_POST['name'] == "om"){
+		$hp = 15; $mp = 25; $atk = 2; $def = 2; $int = 4; 
+		$spr = 3; $move = 2; $crit = 4; $eva = 1;
+	}
 
-				$w1id = 0;
-				$w2id = 0;
-				$arid = 0;
-				$acid = 0;
-				$said = 0;
-				foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['w1']) as $w1) {
-					$w1id = $w1['id'];
-				}
-				if ($_POST['w2'] !== 0)
-				foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['w2']) as $w2) {
-					$w2id = $w2['id'];
-				}
-				else { $w2id = NULL; }
-				foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['ar']) as $ar) {
+	$w1id = 0;
+	$w2id = 0;
+	$arid = 0;
+	$acid = 0;
+	$said = 0;
+	foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['w1']) as $w1) {
+		$w1id = $w1['id'];
+	}
+	if ($_POST['w2'] !== 0)
+		foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['w2']) as $w2) {
+		$w2id = $w2['id'];
+	}
+	else { $w2id = NULL; }
+
+	foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['ar']) as $ar) {
 					$arid = $ar['id'];
-				}
-				foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['ac']) as $ac) {
-					$acid = $ac['id'];
-				}
-				foreach ($db->query("SELECT * FROM Abilities WHERE id = " . $_POST['sa']) as $sa) {
-					$said = $sa['id'];
-				}
+	}
+	foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['ac']) as $ac) {
+		$acid = $ac['id'];
+	}
+	foreach ($db->query("SELECT * FROM Abilities WHERE id = " . $_POST['sa']) as $sa) {
+		$said = $sa['id'];
+	}
 
-				$s1name = $_POST['s1name'];
-				$s1dmg = "";
-				if ($_POST['s1dmg'] !== 0) {
-					$s1dmg = $_POST['s1dmg'] . "% ". $_POST['s1type'] ." Damage. ";
-				}
+	$s1name = $_POST['s1name'];
+	$s1dmg = "";
+	if ($_POST['s1dmg'] !== 0) {
+		$s1dmg = $_POST['s1dmg'] . "% ". $_POST['s1type'] ." Damage. ";
+	}
 
-				$s1stat = $_POST['s1stat'];
-				$s1effect = "";
-				if ($_POST['s1status'] !== 0) {
-					$s1effect .= "Chance of " . $_POST['s1status'] . ". ";
-				}
-				if ($_POST['s1buff'] !== 0) {
-					$s1effect .= $_POST['s1buff'] . " " . $_POST['s1buffstat'] . " by " . $_POST['s1buffamt'] . "%. ";
-				}
-				if ($_POST['s1other'] !== 0) {
-					$s1effect .= $_POST['s1other'] . ".";
-				}
+	$s1stat = $_POST['s1stat'];
+	$s1effect = "";
+	if ($_POST['s1status'] !== 0) {
+		$s1effect .= "Chance of " . $_POST['s1status'] . ". ";
+	}
+	if ($_POST['s1buff'] !== 0) {
+		$s1effect .= $_POST['s1buff'] . " " . $_POST['s1buffstat'] . " by " . $_POST['s1buffamt'] . "%. ";
+	}
+	if ($_POST['s1other'] !== 0) {
+		$s1effect .= $_POST['s1other'] . ".";
+	}
 				
-				$s1range = $_POST['s1range'];
-				$s1mp = $_POST['s1mp'];
+	$s1range = $_POST['s1range'];
+	$s1mp = $_POST['s1mp'];
 
-				$s2name = $_POST['s2name'];
-				$s2dmg = "";
-				if ($_POST['s2dmg'] !== 0) {
-					$s2dmg = $_POST['s2dmg'] . "% ". $_POST['s2type'] ." Damage. ";
-				}
+	$s2name = $_POST['s2name'];
+	$s2dmg = "";
+	if ($_POST['s2dmg'] !== 0) {
+		$s2dmg = $_POST['s2dmg'] . "% ". $_POST['s2type'] ." Damage. ";
+	}
 
-				$s2stat = $_POST['s2stat'];
-				$s2effect = "";
-				if ($_POST['s2status'] !== 0) {
-					$s2effect .= "Chance of " . $_POST['s2status'] . ". ";
-				}
-				if ($_POST['s2buff'] !== 0) {
-					$s2effect .= $_POST['s2buff'] . " " . $_POST['s2buffstat'] . " by " . $_POST['s2buffamt'] . "%. ";
-				}
-				if ($_POST['s2other'] !== 0) {
-					$s2effect .= $_POST['s2other'] . ".";
-				}
-				$s2range = $_POST['s2range'];
-				$s2mp = $_POST['s2mp'];
+	$s2stat = $_POST['s2stat'];
+	$s2effect = "";
+	if ($_POST['s2status'] !== 0) {
+		$s2effect .= "Chance of " . $_POST['s2status'] . ". ";
+	}
+	if ($_POST['s2buff'] !== 0) {
+		$s2effect .= $_POST['s2buff'] . " " . $_POST['s2buffstat'] . " by " . $_POST['s2buffamt'] . "%. ";
+	}
+	if ($_POST['s2other'] !== 0) {
+		$s2effect .= $_POST['s2other'] . ".";
+	}
+	$s2range = $_POST['s2range'];
+	$s2mp = $_POST['s2mp'];
 
-				try
-	{
+	try	{
 	// Add the Scripture
 	// We do this by preparing the query with placeholder values
 		$query = 'INSERT INTO Skills(name, dmg, stat, effect, range, mp) VALUES(:name, :dmg, :stat, :effect, :range, :mp)';
@@ -129,8 +128,8 @@
 	// Now we bind the values to the placeholders. This does some nice things
 	// including sanitizing the input with regard to sql commands.
 		$statement->bindValue(':name', $class);
-		$statement->bindValue(':dmg', $skill1);
-		$statement->bindValue(':stat', $skill2);
+		$statement->bindValue(':skill1', $sk1);
+		$statement->bindValue(':skill2', $sk2);
 		$statement->execute();
 
 		$aaid = $statement->lastInsertId();
