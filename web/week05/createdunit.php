8cd7d4285f53b5145a963158c6bb3341a4777a4b
ziolang <stumpyfletcher@gmail.com>
2019-10-25 15:38:44 -0600

    more work

--- web/week05/createdunit.php
+++ web/week05/createdunit.php
@@ -1,29 +1,6 @@
-<?php include "connect.php";?>
-
-<!DOCTYPE html>
-<head>
-	<link rel="stylesheet" type="text/css" href="week05.css">
-	<meta charset="utf-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-	<div class="container">
-  		<div class="row">
-			<?php include "navbar.php";?>
-		</div>
-	</div>
-	<div class="container">
-  		<div class="row">
-			<div class="col-sm-2">
+<?php include "connect.php";
 
 
-			</div>
-			<div class="search col-sm-8">
-				<?php
 				$db = connect();
 
 				$name = $_POST['name'];
@@ -48,35 +25,26 @@
 					$spr = 3; $move = 2; $crit = 4; $eva = 1;
 				}
 
-				echo "$name<br/>$class<br/>$lvl<br/>$exp<br/>
-				$hp<br/>$mp<br/>$atk<br/>$def<br/>$int<br/>
-				$spr<br/>$move<br/>$crit<br/>$eva<br/>";
-
 				$w1id = 0;
 				$w2id = 0;
 				$arid = 0;
 				$acid = 0;
 				$said = 0;
 				foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['w1']) as $w1) {
-					echo 'Weapon1: ' . $w1['name'] . " (" . $w1['effect'] . ")<br/>";
 					$w1id = $w1['id'];
 				}
 				if ($_POST['w2'] !== 0)
 				foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['w2']) as $w2) {
-					echo 'Weapon2: ' . $w2['name'] . " (" . $w2['effect'] . ")<br/>";
 					$w2id = $w2['id'];
 				}
 				else { $w2id = NULL; }
 				foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['ar']) as $ar) {
-					echo 'Armor: ' . $ar['name'] . " (" . $ar['effect'] . ")<br/>";
 					$arid = $ar['id'];
 				}
 				foreach ($db->query("SELECT * FROM Equips WHERE id = " . $_POST['ac']) as $ac) {
-					echo 'Accessory: ' . $ac['name'] . " (" . $ac['effect'] . ")<br/>";
 					$acid = $ac['id'];
 				}
 				foreach ($db->query("SELECT * FROM Abilities WHERE id = " . $_POST['sa']) as $sa) {
-					echo 'S-Ability: ' . $sa['name'] . ": " . $sa['effect'] . "<br/>";
 					$said = $sa['id'];
 				}
 
@@ -101,8 +69,6 @@
 				$s1range = $_POST['s1range'];
 				$s1mp = $_POST['s1mp'];
 
-				echo "<b>+ $s1name:</b> $s1dmg $s1effect Range: $s1range. $s1mp MP.<br/>";
-
 				$s2name = $_POST['s2name'];
 				$s2dmg = "";
 				if ($_POST['s2dmg'] !== 0) {
@@ -123,62 +89,60 @@
 				$s2range = $_POST['s2range'];
 				$s2mp = $_POST['s2mp'];
 
-				echo "<b>+ $s2name:</b> $s2dmg $s2effect Range: $s2range. $s2mp MP.<br/>";
-
 				try
-{
+	{
 	// Add the Scripture
 	// We do this by preparing the query with placeholder values
-	$query = 'INSERT INTO Skills(name, dmg, stat, effect, range, mp) VALUES(:name, :dmg, :stat, :effect, :range, :mp)';
-	$statement = $db->prepare($query);
-	$s1damage = $_POST['s1dmg'];
-	$s2damage = $_POST['s1dmg'];
+		$query = 'INSERT INTO Skills(name, dmg, stat, effect, range, mp) VALUES(:name, :dmg, :stat, :effect, :range, :mp)';
+		$statement = $db->prepare($query);
+		$s1damage = $_POST['s1dmg'];
+		$s2damage = $_POST['s1dmg'];
 	// Now we bind the values to the placeholders. This does some nice things
 	// including sanitizing the input with regard to sql commands.
-	$statement->bindValue(':name', $s1name);
-	$statement->bindValue(':dmg', $s1damage);
-	$statement->bindValue(':stat', $s1stat);
-	$statement->bindValue(':effect', $s1effect);
-	$statement->bindValue(':range', $s1range);
-	$statement->bindValue(':mp', $s1mp);
-	$statement->execute();
+		$statement->bindValue(':name', $s1name);
+		$statement->bindValue(':dmg', $s1damage);
+		$statement->bindValue(':stat', $s1stat);
+		$statement->bindValue(':effect', $s1effect);
+		$statement->bindValue(':range', $s1range);
+		$statement->bindValue(':mp', $s1mp);
+		$statement->execute();
 
-	$sk1 = $statement->lastInsertId();
+		$sk1 = $statement->lastInsertId();
 
-	$query = 'INSERT INTO Skills(name, dmg, stat, effect, range, mp) VALUES(:name, :dmg, :stat, :effect, :range, :mp)';
-	$statement = $db->prepare($query);
+		$query = 'INSERT INTO Skills(name, dmg, stat, effect, range, mp) VALUES(:name, :dmg, :stat, :effect, :range, :mp)';
+		$statement = $db->prepare($query);
 	// Now we bind the values to the placeholders. This does some nice things
 	// including sanitizing the input with regard to sql commands.
-	$statement->bindValue(':name', $s2name);
-	$statement->bindValue(':dmg', $s2damage);
-	$statement->bindValue(':stat', $s2stat);
-	$statement->bindValue(':effect', $s2effect);
-	$statement->bindValue(':range', $s2range);
-	$statement->bindValue(':mp', $s2mp);
-	$statement->execute();
+		$statement->bindValue(':name', $s2name);
+		$statement->bindValue(':dmg', $s2damage);
+		$statement->bindValue(':stat', $s2stat);
+		$statement->bindValue(':effect', $s2effect);
+		$statement->bindValue(':range', $s2range);
+		$statement->bindValue(':mp', $s2mp);
+		$statement->execute();
 
-	$sk2 = $statement->lastInsertId();
+		$sk2 = $statement->lastInsertId();
 
-	$query = 'INSERT INTO SkillSet(name, skill1, skill2) VALUES(:name, :skill1, :skill2)';
-	$statement = $db->prepare($query);
+		$query = 'INSERT INTO SkillSet(name, skill1, skill2) VALUES(:name, :skill1, :skill2)';
+		$statement = $db->prepare($query);
 
 	// Now we bind the values to the placeholders. This does some nice things
 	// including sanitizing the input with regard to sql commands.
-	$statement->bindValue(':name', $class);
-	$statement->bindValue(':dmg', $skill1);
-	$statement->bindValue(':stat', $skill2);
-	$statement->execute();
+		$statement->bindValue(':name', $class);
+		$statement->bindValue(':dmg', $skill1);
+		$statement->bindValue(':stat', $skill2);
+		$statement->execute();
 
-	$aaid = $statement->lastInsertId();
+		$aaid = $statement->lastInsertId();
 
-	$query = 'INSERT INTO Units(name, class, aability, sability, weapon1, weapon2, armor, accessory, lvl, exp, hp, mp, atk, def, int, spr, move, crit, eva) 
-		VALUES(:name, :class, :aability, :sability, :weapon1, 
+		$query = 'INSERT INTO Units(name, class, aability, sability, weapon1, weapon2, armor, accessory, lvl, exp, hp, mp, atk, def, int, spr, move, crit, eva) 
+			VALUES(:name, :class, :aability, :sability, :weapon1, 
 			:weapon2, :armor, :accessory, :lvl,
 			:exp, :hp, :mp, :atk, :def, :int, :spr, :move, :crit, :eva)';
-	$statement = $db->prepare($query);
+		$statement = $db->prepare($query);
 	// Now we bind the values to the placeholders. This does some nice things
 	// including sanitizing the input with regard to sql commands.
-	$statement->bindValue(':name', $name);
+		$statement->bindValue(':name', $name);
 	$statement->bindValue(':class', $class);
 	$statement->bindValue(':aability', $aaid);
 	$statement->bindValue(':sability', $said);
@@ -212,12 +176,3 @@ die();
 
 				
 				?>
-			</div>
-			<div class="col-sm-2">
-
-
-			</div>
-		</div>
-
-	</div>
-</body>
