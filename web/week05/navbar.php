0d5df3ac4d1673d9f3d96b022999e409ca289199
ziolang <stumpyfletcher@gmail.com>
2019-10-26 22:43:03 -0600

    finished

--- web/week05/navbar.php
+++ web/week05/navbar.php
@@ -3,7 +3,8 @@
   		<div class="row">
   		<ul class="nav col-sm-12">
   		<li><a href="home.php">Home</a></li>
-  		<li><a href="search.php">Units</a></li>
+  		<li><a href="search.php">Search Units</a></li>
+  		<li><a href="newunit.php">New Unit</a></li>
   		<li><a>Grids</a></li>
 	</ul>
 	</div>
