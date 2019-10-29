4ba81eac1b2c6a4b3adfcb0b835092c7303d3634
ziolang <stumpyfletcher@gmail.com>
2019-10-28 22:33:08 -0600

    work work

--- web/week05/checklvlup.js
+++ web/week05/checklvlup.js
@@ -2,7 +2,7 @@ function checklvlup() {
 	var statpts = parseInt(document.getElementById("caption").innerHTML, 10);
 
 	if (statpts > 0) {
-		alert("Spend all of your stat points.")
+		alert("Spend all of your stat points.");
 		return false;
 	}
 }
