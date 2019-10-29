13a4bcae0928e577e6f482de0351735928a2df07
ziolang <stumpyfletcher@gmail.com>
2019-10-29 09:01:14 -0600

    work work

--- web/week07.js
+++ web/week07.js
@@ -1,6 +1,6 @@
 function isIdentical() {
-	var item1 = document.getElementById("").value
-	var item2 = document.getElementById("").value
+	var item1 = document.getElementById("p1").value
+	var item2 = document.getElementById("p2").value
 
 	if (item1.length != item2.length) {
 		return false;
@@ -14,8 +14,8 @@ function isIdentical() {
 }
 
 function checkPassword() {
-	var item1 = document.getElementById("").value
-	var item2 = document.getElementById("").value
+	var item1 = document.getElementById("p1").value
+	var item2 = document.getElementById("p2").value
 	var isSame = true;
 	var numCount = 0;
 	var charCount = 0;
