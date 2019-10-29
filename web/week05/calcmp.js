c4b4cd846f082a9cb6b3970aea279d2bcffcd6f8
ziolang <stumpyfletcher@gmail.com>
2019-10-29 09:35:11 -0600

    work work

--- web/week05/calcmp.js
+++ web/week05/calcmp.js
@@ -15,18 +15,8 @@ function calculateMP() {
 		mp1 += temp;
 		temp = 0;
 	}
-	if (document.getElementsByName("s2dmg")[0].value >= 100) {
-		temp = (document.getElementsByName("s2dmg")[0].value - 100) / 10;
-
-		if (document.getElementsByName("s2dmg")[0].value >= 200)
-			temp *= 2;
-
-		mp2 += temp;
-		temp = 0;
-	}
 
 	if (document.getElementsByName("s1status")[0].value != 0) {mp1 += 2;}
-	if (document.getElementsByName("s2status")[0].value != 0) {mp2 += 2;}
 	
 	if (document.getElementsByName("s1buff")[0].value != 0) {
 		temp = document.getElementsByName("s1buffamt")[0].value / 10;
@@ -38,4 +28,25 @@ function calculateMP() {
 		mp1 += temp;
 		temp = 0;
 	}
+
+	other = document.getElementsByName("s1other")[0];
+	if (other.value != 0) {
+		r = document.getElementsByName("s1range")[0].value;
+		if (other.value == "Knockback") { mp1 += 2; }
+		if (other.value == "Hits 2 times") { mp1 *= 2;}
+		if (other.value == "1 cell AoE") { mp1 += 2; }
+		if (other.value == "Line-shape area") { mp1 += (r - 2); }
+		if (other.value == "2 cell AoE") { mp1 += 5; }
+		if (other.value == "Cone-shape area") { 
+			mp1 += (r * (r - 1));
+		}
+	}
+
+	r = document.getElementsByName("s1range")[0].value;
+	if (r >= 3) {
+		mp1 += r - 2;
+	}
+
+	document.getElementById("s1mpview").innerHTML = mp1;
+	document.getElementsByName("s1mp")[0].value = mp1;
 }
