fa5e27912b1822d7618fb763c721fdf7c17cc663
ziolang <stumpyfletcher@gmail.com>
2019-11-02 19:52:12 -0600

    finalize lvl up

--- web/week05/unit.php
+++ web/week05/unit.php
@@ -74,11 +74,11 @@
 						"<br/><strong>Move:</strong> " . $unit['move'] . 
 						"</div>
 								<div class=\"search col-sm-2\">
-									<button type=\"button\" href=\"levelup.php?id=\"" . $_GET['id'] . ">LEVEL UP</button>
+									<button type=\"button\" class=\"unitbtn\" href=\"levelup.php?id=\"" . $_GET['id'] . ">LEVEL UP</button>
 									<br/>
 									<br/>
 									<br/>
-									<button type=\"button\" href=\"delete.php\">DELETE UNIT</button>
+									<button type=\"button\" class=\"unitbtn\" href=\"delete.php\">DELETE UNIT</button>
 								</div>
 								<div class=\"col-sm-1\"></div>
 							</div>
